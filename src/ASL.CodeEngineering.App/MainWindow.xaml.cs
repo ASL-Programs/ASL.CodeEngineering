5a4bc57f19ac78e70e3a8b540acc19def0be78b7
Vusal Mastaliyev <32294930+Admiral4828@users.noreply.github.com>
2025-06-25 17:19:25 +0400

    Initialize project structure with WPF skeleton

---
+++ src/ASL.CodeEngineering.App/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ASL.CodeEngineering
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
